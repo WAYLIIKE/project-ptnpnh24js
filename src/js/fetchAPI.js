52760486a53b4bdf2daadd535bd5684cd8c070d5
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-21 04:34:18 +0100

    Project with configuration and base files created

---
+++ src/js/fetchAPI.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+export {
+  getAllProducts,
+  getProductByID,
+  getAllCategories,
+  getAllPopular,
+  getAllDiscount,
+};
+
+async function getAllProducts(obj) {
+  const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
+  const queryParams = new URLSearchParams(obj);
+  try {
+    const result = await axios.get(`${BASE_URL}?${queryParams}`);
+    return result.data;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function getAllCategories() {
+  const BASE_URL = 'https://food-boutique.b.goit.study/api/products/categories';
+  try {
+    const result = await axios.get(`${BASE_URL}`);
+    return result.data;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function getAllPopular() {
+  const BASE_URL = 'https://food-boutique.b.goit.study/api/products/popular';
+  try {
+    const result = await axios.get(`${BASE_URL}`);
+    return result.data;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function getAllDiscount() {
+  const BASE_URL = 'https://food-boutique.b.goit.study/api/products/discount';
+  try {
+    const result = await axios.get(`${BASE_URL}`);
+    return result.data;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function getProductByID(id) {
+  const BASE_URL = 'https://food-boutique.b.goit.study/api/products/';
+  try {
+    const result = await axios.get(`${BASE_URL}${id}`);
+    return result.data;
+  } catch (error) {
+    return error.message;
+  }
+}
