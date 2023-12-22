ca8cca9784a81359f8485aef290ff109390d4392
IrinaLender27 <irina.lender27@gmail.com>
2023-12-22 20:49:10 +0200

    Update markupPopular.js

--- src/js/markupPopular.js
+++ src/js/markupPopular.js
@@ -1,6 +1,10 @@
 import { getAllPopular } from './fetchAPI';
 
 const popularList = document.querySelector('.popular-list');
+// const popularBtn = document.querySelector('.popularbtn-basket');
+// console.log(popularBtn);
+const PRODUCT_KEY = 'basket';
+const basketArr = JSON.parse(localStorage.getItem(PRODUCT_KEY)) ?? [];
 
 async function getDataAllPopular() {
   try {
@@ -19,8 +23,8 @@ getDataAllPopular()
   });
 
 function createMarkUpPopular(arr) {
-  const markUp = arr.map(({ img, name, category, size, popularity }) => {
-    return `<li class="popular-item">
+  const markUp = arr.map(({ id, img, name, category, size, popularity }) => {
+    return `<li class="popular-item" data-id='${id}'>
     <div class="popular-containar">
     <div class="popular-image-container">
     <img class="popular-image" src="${img}" alt="${name}" width="56px" height="56px">
@@ -40,3 +44,18 @@ function createMarkUpPopular(arr) {
   });
   popularList.insertAdjacentHTML('beforeend', markUp.join(''));
 }
+
+popularList.addEventListener('click', onClick);
+function onClick(event) {
+  console.log(event.target);
+  if (event.target.classList.contains('.popularbtn-basket')) {
+    const product = findProduct(event.target);
+    basketArr.push(product);
+    localStorage.setItem(PRODUCT_KEY, JSON.stringify(basketArr));
+  }
+}
+function findProduct(element) {
+  const productId = Number(element.closest('.popular-item')).dataset.id;
+  return Array.find(({ id }) => id === productId);
+}
+console.log(onClick());
