421d00c6350feb07187c705705927b27a1d46f55
IrinaLender27 <irina.lender27@gmail.com>
2023-12-22 23:26:34 +0200

    update popular-prodact

--- src/js/markupPopular.js
+++ src/js/markupPopular.js
@@ -1,8 +1,6 @@
 import { getAllPopular } from './fetchAPI';
 
 const popularList = document.querySelector('.popular-list');
-// const popularBtn = document.querySelector('.popularbtn-basket');
-// console.log(popularBtn);
 const PRODUCT_KEY = 'basket';
 const basketArr = JSON.parse(localStorage.getItem(PRODUCT_KEY)) ?? [];
 
@@ -25,20 +23,20 @@ getDataAllPopular()
 function createMarkUpPopular(arr) {
   const markUp = arr.map(({ id, img, name, category, size, popularity }) => {
     return `<li class="popular-item" data-id='${id}'>
-    <div class="popular-containar">
+    <div class="popular-card">
     <div class="popular-image-container">
     <img class="popular-image" src="${img}" alt="${name}" width="56px" height="56px">
     </div>
     <div class="info-popular-card">
     <h4 class="popular-name">${name}</h4>
-    <p class="info-popular-item">
-    <b>Category: ${category}</b></p>
-    <p class="info-popular-item">
-    <b>Size: ${size}</b></p>
-    <p class="info-popular-item">
-    <b>Popularity:${popularity}</b></p>
+    <p class="info-popular-item">Category:
+    <b>${category}</b></p>
+    <p class="info-popular-item">Size: 
+    <b>${size}</b></p>
+    <p class="info-popular-item">Popularity:
+    <b>${popularity}</b></p>
     </div>
-    <button class="popularbtn-basket" type="button">Візок</button>
+    <button class="popularbtn-basket" type="button"></button>
     </div>
     </li>`;
   });
@@ -47,7 +45,6 @@ function createMarkUpPopular(arr) {
 
 popularList.addEventListener('click', onClick);
 function onClick(event) {
-  console.log(event.target);
   if (event.target.classList.contains('.popularbtn-basket')) {
     const product = findProduct(event.target);
     basketArr.push(product);
