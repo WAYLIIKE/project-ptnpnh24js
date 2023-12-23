cfd52a5c24345542d28d018e8059e09c58d03889
WAYLIIKE <116974702+WAYLIIKE@users.noreply.github.com>
2023-12-23 02:34:44 +0000

    Deploying to gh-pages from @ WAYLIIKE/project-ptnpnh24js@43a2d0f523a33f089d598264e5c526748340ec3a 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import{a as t,r as a,u as e}from"./assets/header-f286fd61.js";t.addEventListener("click",function(){addItemToLocalArray(Math.random())});a.addEventListener("click",function(){removeItemFromLocalArray(0)});e();
+//# sourceMappingURL=commonHelpers.js.map
