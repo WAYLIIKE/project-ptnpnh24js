3e271d484b4bab048955fb7875b8bf29920067b6
Melodica4585 <melodica4585@gmail.com>
2023-12-23 20:59:32 +0200

    added cart and script

--- src/js/basket.js
+++ src/js/basket.js
@@ -11,6 +11,21 @@ addItemButton.addEventListener('click', function () {
 removeItemButton.addEventListener('click', function () {
   removeItemFromLocalArray(0);
 });
-updateItemCountDisplay();
+updateItemCountDisplay(); 
 
 ////////////////////////////////////////////////////////////////////////////
+
+(() => {
+  const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+  refs.modal.classList.toggle("is-hidden");
+  }
+})();
