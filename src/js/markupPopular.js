ea9e20dd73eeaed21f48bcd69d040dadb076dc2a
IrinaLender27 <irina.lender27@gmail.com>
2023-12-24 01:05:46 +0200

    update

--- src/js/markupPopular.js
+++ src/js/markupPopular.js
@@ -1,8 +1,9 @@
 import { getAllPopular } from './fetchAPI';
 
+const iconShop = './img/icons/sprite.svg#icon-shop';
+const iconCheck = './img/icons/sprite.svg#icon-check';
+
 const popularList = document.querySelector('.popular-list');
-const PRODUCT_KEY = 'basket';
-const basketArr = JSON.parse(localStorage.getItem(PRODUCT_KEY)) ?? [];
 
 async function getDataAllPopular() {
   try {
@@ -38,7 +39,15 @@ function createMarkUpPopular(arr) {
     <b>${popularity}</b></p>
     </div>
     </div>
-    <button class="popularbtn-basket" type="button">
+    <button class="popularbtn-basket " type="button">
+   
+    <svg class = "icon-check is-hidden "  width="12" height="12">
+    <use href="${iconCheck}"></use></svg>
+   
+   
+    <svg class="icon-shop" width="12" height="12" >
+    <use href="${iconShop}"></use></svg>
+    
     </button>
     </div>
     </li>`;
@@ -50,13 +59,20 @@ popularList.addEventListener('click', onClick);
 function onClick(event) {
   const targetButton = event.target.closest('.popularbtn-basket');
   if (!targetButton) return;
-  const card = targetButton.closest('.popular-item');
-  const id = card.dataset.productId;
+  const card = event.target.closest('.popular-item');
+  const id = card.dataset.id;
   const basket = getBasketLocalStorage();
+
   if (basket.includes(id)) return;
+
+  // ПЕРЕВІР ТУТ
+  // const check = document.querySelectorAll('.icon-check');
+  // const shop = document.querySelector('.icon-shop');
+  // shop.classList.add("is-hidden");
+  // check.classList.remove('is-hidden');
+
   basket.push(id);
   setBasketLocalStorage(basket);
-  console.log(id);
 }
 function getBasketLocalStorage() {
   const cardDataJSONE = localStorage.getItem('basket');
@@ -64,21 +80,4 @@ function getBasketLocalStorage() {
 }
 function setBasketLocalStorage(basket) {
   localStorage.setItem('basket', JSON.stringify(basket));
-}
-// function onClick(event) {
-//   if (event.target.classList.contains('.popularbtn-basket')) {
-//     const product = findProduct(event.target);
-//     basketArr.push(product);
-//     localStorage.setItem(PRODUCT_KEY, JSON.stringify(basketArr));
-//   }
-// }
-// function findProduct(element) {
-//   const productId = Number(element.closest('.popular-item')).dataset.id;
-//   return Array.find(({ id }) => id === productId);
-// }
-// console.log(onClick());
-
-// Проверка кнопок на активность.Наличие в корзине . Изменить иконку кнопки
-function activButton(basket) {
-  const buttons = document.querySelectorAll('.popularbtn-basket');
 }
