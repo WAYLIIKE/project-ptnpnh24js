eb6db9436a23eb76e293c7284a9df1a148d4f27c
Walenty Sawicki <116974702+WAYLIIKE@users.noreply.github.com>
2023-12-23 20:07:14 +0000

    review changes

--- src/js/basket.js
+++ src/js/basket.js
@@ -14,18 +14,3 @@ removeItemButton.addEventListener('click', function () {
 updateItemCountDisplay(); 
 
 ////////////////////////////////////////////////////////////////////////////
-
-(() => {
-  const refs = {
-  openModalBtn: document.querySelector("[data-modal-open]"),
-  closeModalBtn: document.querySelector("[data-modal-close]"),
-  modal: document.querySelector("[data-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");
-  }
-})();
