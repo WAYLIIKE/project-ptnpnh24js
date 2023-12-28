4d06ae8ff2c1d42a843754f6718d47e108e6e119
WAYLIIKE <116974702+WAYLIIKE@users.noreply.github.com>
2023-12-28 01:15:41 +0000

    Deploying to gh-pages from @ WAYLIIKE/project-ptnpnh24js@9bc030e3c7ebb4dd133e82301399265b7718b8be 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,17 +1,17 @@
-import{u as n,f as u,s as d}from"./assets/footer-0ca8e3a3.js";import{a as h}from"./assets/vendor-2dcf4ad5.js";async function g(){const t="https://food-boutique.b.goit.study/api/products/popular";try{return(await h.get(`${t}`)).data}catch(s){return s.message}}const i="/project-ptnpnh24js/assets/sprite-fe336fca.svg",p=document.querySelector(".popular-list");async function f(){try{return await g()}catch(t){console.log(t)}}function m(t){const s=t.map(({_id:r,img:a,name:e,category:o,size:c,popularity:l})=>`<li class="popular-item" data-id='${r}'>
+import{u as r,f as h,s as g}from"./assets/footer-46b6302e.js";import{a as u}from"./assets/vendor-2dcf4ad5.js";async function m(){const t="https://food-boutique.b.goit.study/api/products/popular";try{return(await u.get(`${t}`)).data}catch(s){return s.message}}async function f(){const t="https://food-boutique.b.goit.study/api/products/discount";try{return(await u.get(`${t}`)).data}catch(s){return s.message}}const i="/project-ptnpnh24js/assets/sprite-fe336fca.svg",l=document.querySelector(".popular-list");async function b(){try{return await m()}catch(t){console.log(t)}}function v(t){const s=t.map(({_id:c,img:o,name:e,category:a,size:n,popularity:p})=>`<li class="popular-item" data-id='${c}'>
     <div class="popular-card">
     <div class="popular-image-container">
-    <img class="popular-image" src="${a}" alt="${e}" width="56px" height="56px">
+    <img class="popular-image" src="${o}" alt="${e}" width="56px" height="56px">
     </div>
     <div class="info-popular-card">
     <h4 class="popular-name">${e}</h4>
     <p class="info-popular-item">Category:
-    <b>${o}</b></p>
+    <b>${a}</b></p>
     <div class="add">
     <p class="info-popular-item">Size: 
-    <b>${c}</b></p>
+    <b>${n}</b></p>
     <p class="info-popular-item">Popularity:
-    <b>${l}</b></p>
+    <b>${p}</b></p>
     </div>
     </div>
     <button class="popularbtn-basket " type="button">
@@ -24,5 +24,28 @@ import{u as n,f as u,s as d}from"./assets/footer-0ca8e3a3.js";import{a as h}from
 
     </button>
     </div>
-    </li>`);p.insertAdjacentHTML("beforeend",s.join(""))}function b(t){const s=t.target.closest(".popularbtn-basket");if(!s)return;const a=t.target.closest(".popular-item").dataset.id,e=k();if(e.includes(a))return;const o=s.querySelector(".icon-check");s.querySelector(".icon-shop").classList.add("is-hidden"),o.classList.remove("is-hidden"),e.push(a),v(e),n()}function k(){const t=localStorage.getItem("basket");return t?JSON.parse(t):[]}function v(t){localStorage.setItem("basket",JSON.stringify(t))}n();u.addEventListener("submit",d);f().then(m).catch(t=>{console.log(t)});p.addEventListener("click",b);
+    </li>`);l.insertAdjacentHTML("beforeend",s.join(""))}function k(t){const s=t.target.closest(".popularbtn-basket");if(!s)return;const o=t.target.closest(".popular-item").dataset.id,e=S();if(e.includes(o))return;const a=s.querySelector(".icon-check");s.querySelector(".icon-shop").classList.add("is-hidden"),a.classList.remove("is-hidden"),e.push(o),y(e),r()}function S(){const t=localStorage.getItem("basket");return t?JSON.parse(t):[]}function y(t){localStorage.setItem("basket",JSON.stringify(t))}const d=document.querySelector(".discount-list");async function $(){try{const t=await f(),s=L(t,2);d.insertAdjacentHTML("beforeend",w(s))}catch(t){console.log(t.message)}}function L(t,s){return t.sort(()=>.5-Math.random()).slice(0,s)}function w(t){return t.map(({name:s,img:c,price:o,_id:e})=>`
+    <li data-id="${e}" class="discount-product">
+    <div class="discount-image">
+        <svg class="discount-list-item" width="60" heigh="60">
+        <use href="./img/icons/sprite.svg#icon-discount"></use>
+        </svg>
+        <img src="${c}" alt="name" class="discount-image-cart"/>
+    </div>
+    <div class="discount-info">
+        <p class="discount-title-name">${s}</p>
+        <div class="discount-info-cart">
+            <p class="discount-price">$${o}</p>
+            <button class="discount-btn" type="button">
+
+            <svg class = "icon-check is-hidden"  width="12" height="12">
+            <use href="${i}#icon-check"></use></svg>
+
+            <svg class="icon-shop" width="12" height="12">
+            <use href="${i}#icon-shop"></use></svg>
+
+        </button>
+        </div>
+    </div>
+    </li>`).join("")}d.addEventListener("click",q);function q(t){if(t.target===t.currentTarget)return;const c=t.target.closest(".discount-product").dataset.id,o=t.target.closest(".discount-btn");if(!o)return;const e=A();if(e.includes(c))return;const a=o.querySelector(".icon-check");o.querySelector(".icon-shop").classList.add("is-hidden"),a.classList.remove("is-hidden"),e.push(c),B(e),r()}function A(){const t=localStorage.getItem("basket");return t?JSON.parse(t):[]}function B(t){localStorage.setItem("basket",JSON.stringify(t))}r();h.addEventListener("submit",g);b().then(v).catch(t=>{console.log(t)});l.addEventListener("click",k);$();
 //# sourceMappingURL=commonHelpers2.js.map
